c6d0e327e1b484f98bf2bd1bd583e1686b32600a
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2025-06-06 10:30:51 +0300

    check linters

--- src/main/java/ru/tyatyushkin/telegram/Weather.java
+++ src/main/java/ru/tyatyushkin/telegram/Weather.java
@@ -7,8 +7,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Weather {
     private final String token;
